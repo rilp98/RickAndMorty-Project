b409bb716d0a4e11306358ddd746fef356949bf0
rilp98 <paolaanttonelar23@gmail.com>
2022-01-18 19:20:53 -0400

    Add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState,useEffect} from 'react';
-import './App.css';
 
 function App() {
   const [item, setItem] = useState([]);
-  const [data,setData] = useState('Yaarb');
+  const [data,setData] = useState('rick');
   
   const handleData = (e) =>{
   setData(e.target.value);
-   console.log(data);
   }
 
   const handleSubmit = (e)=>{
@@ -25,26 +23,28 @@ function App() {
   }
 
   useEffect( ()=>{
-      
       fetchData();
-       console.log(item);
-
    },[]);
   
   
   return (
-    <div >
+    <div className='h-screen bg-[#5554d4] flex flex-col items-center'>
+
+      <h1  className='mt-10' >Rick and Morty Search App</h1>
 
       <div>
 
-      <input placeholder="search" type="text" onChange={handleData} value={data} />
-      <button onClick={handleSubmit} >Add</button>
+      <input placeholder='Search' type='text' onChange={handleData} value={data}  
+      className="bg-[] my-10 h-10 px-5 pr-10 text-sm focus:outline-none" />
+
+      <button onClick={handleSubmit} className=" bg-[#ff7143] w-10 h-10" >Add</button>
 
       </div>
 
      {item.map( (i)=>{
-       return <img key={i.id} src={i.image} alt="imagen" />
+       return <img key={i.id} src={i.image} alt="imagen" className='w-2 h-4' />
      } )}
+
     </div>
   )
 }
