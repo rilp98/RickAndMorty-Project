112ae78c2e0275a72255f54fb28997e1518fb11a
rilp98 <paolaanttonelar23@gmail.com>
2022-01-18 23:04:40 -0400

    Change the style

--- src/App.jsx
+++ src/App.jsx
@@ -28,22 +28,26 @@ function App() {
   
   
   return (
-    <div className='h-screen bg-[#5554d4] flex flex-col items-center'>
+    <div className='h-full bg-indigo-200 flex flex-col items-center'>
 
-      <h1  className='mt-10' >Rick and Morty Search App</h1>
+      <h1  className='mt-10 text-3xl' >Rick and Morty Search App</h1>
 
       <div>
 
       <input placeholder='Search' type='text' onChange={handleData} value={data}  
-      className="bg-[] my-10 h-10 px-5 pr-10 text-sm focus:outline-none" />
+      className="bg-[] w-96 rounded-l-lg my-10 h-10 px-5 pr-10 text-sm focus:outline-none" />
 
-      <button onClick={handleSubmit} className=" bg-[#ff7143] w-10 h-10" >Add</button>
+      <button onClick={handleSubmit} className='bg-[#5554d4] w-12 h-10 rounded-r-lg' >Add</button>
 
       </div>
+    
+    <div className='grid grid-cols-6 '>
 
      {item.map( (i)=>{
-       return <img key={i.id} src={i.image} alt="imagen" className='w-2 h-4' />
+       return <img key={i.id} src={i.image} alt="imagen" className='w-40 h-40' />
      } )}
+    
+    </div>
 
     </div>
   )
