463eb9307dda1f0762d103fd45bab15086806f75
rilp98 <paolaanttonelar23@gmail.com>
2022-01-20 23:00:15 -0400

    Add style

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
   
   
   return (
-    <div className='h-full flex flex-col items-center text-gray-700 dark:bg-zinc-900'>
+    <div className='h-full flex flex-col items-center text-gray-700 dark:bg-slate-900'>
 
       <h1  className='mt-10 text-4xl font-semibold dark:text-indigo-200' >Rick and Morty Search App</h1>
 
@@ -40,15 +40,15 @@ function App() {
 
       <input placeholder='Search a chapter' type='text' onChange={handleData} value={data}  
       className='w-96 h-10 bg-indigo-200 placeholder-slate-600 rounded-l-lg px-5 pr-10 
-      text-sm my-12 focus: outline-none dark:bg-gray-700 dark:opacity-60 dark:text-indigo-200 '/>
+      text-sm font-semibold my-12 focus: outline-none dark:bg-gray-700 dark:opacity-60 dark:text-indigo-200 '/>
 
       <button onClick={handleSubmit} className='bg-indigo-600 text-indigo-100 w-14 h-10 
       rounded-r-lg hover:bg-indigo-700 '> Add </button>
       
-      <button onClick={handleDarkMode} className='bg-indigo-600 dark:bg-emerald-800' >dark mode</button>
+      <button onClick={handleDarkMode} className=' w-24 h-10 ml-4 rounded-lg font-semibold
+      outline outline-offset-2 outline-indigo-900 text-indigo-900 dark:outline-indigo-200 dark:text-indigo-200' >Dark Mode</button>
       
       </div>
-    
 
     <div className='grid grid-cols-5 gap-8 pb-12'>
 
